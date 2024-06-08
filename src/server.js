31f2d0c8fa8458e3d07c2ba676b181e5fb5e63c4
Wellinton Felipe <wellintonfelipe@hotmail.com.br>
2024-06-07 22:55:48 -0300

    create file src/server.js

--- src/server.js
+++ src/server.js
@@ -1,9 +1,44 @@
 import http from "node:http"
 
-const app = http.createServer()
 
-
+const users = []
 const handleRoutes = ((req, res) => {
 
+  const { method, url } = req
+  /**
+   * id
+   * title
+   * description
+   * completed_at default NULL
+   * created_at
+   * update_at 
+   */
+
+  if (method === "GET" && url === "/users") {
+    return res
+      .setHeader('Content-Type', "aplication/json")
+      .writeHeader(200)
+      .end(JSON.stringify(users))
+  }
+
+  if (method === "POST" && url === "/users") {
+    users.push({
+      id: 1,
+      name: "John Doe",
+      email: 'joehdoe@example.com'
+    })
+    return res.writeHeader(201).end()
+  }
+
+  return res.writeHeader(404).end()
+
+
+
 })
-app.listen(3333, () => console.log('servir is running 🚀'))
+
+const app = http.createServer(handleRoutes)
+
+
+
+app.listen(3333,
+  () => console.log('servir is running on PORT 3333 🚀'))
